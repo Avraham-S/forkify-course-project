205d3abca4ee00de70dff32d43fe5b600857d6bf
Yaboy-AY <ayschochet@gmail.com>
2022-08-11 17:43:11 +0200

    add new api key and fixed new recipe form

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const API_KEY = 'a24adceb-f027-42de-bff6-6849434a497a';
+export const API_KEY = '6ff52a25-db38-4966-9efd-7d7ef2f83de2';
 export const MODAL_CLOSE_SEC = 2.5;
